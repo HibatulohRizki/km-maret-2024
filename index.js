e7068959047edb893f01ea2d99b606a2d3ee99e8
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-18 21:31:31 +0700

    membuat function cek angka

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function checkAnkga(angka) {
+    if(angka % 2 === 0){
+        console.log("Bilangan Genap");
+    }else{
+        console.log("Bilangan Ganjil");
+    }
+    
+}
+
+checkAnkga(10); 
