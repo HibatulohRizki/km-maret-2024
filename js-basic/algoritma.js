80ee0490b4649a100dab7f2119a7a4aa5895ac76
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-19 19:40:23 +0700

    Membuat file algoritma dan membuat study kasus 1 yaitu mencari keliling persei panjang

---
+++ js-basic/algoritma.js
@@ -0,0 +1,22 @@
+// ALGORITMA PSEDUOCODE
+/**
+ * STUDY CASE 1
+ * 
+ * TENTUKAN ALGORTIMA DAN PSEUDUCODE UNTUK MENGHITUNG KELILING PERSEGI PANJANG
+ * 
+ * SDTUDY CASE 2
+ * 
+ * TENTUKAN ALGORITMA DAN PSEUDUCODE UNTUK MENGHITUNG LUAS PERMUKAAN TABUNG
+ * 
+ * STUDY CASE 3
+ * 
+ * TENTUKAN ALGORITMA DAN PSEUDUCODE VOLUME 1/4 VOLUME BOLA
+ */
+
+//STUDY CASE  1
+
+let panjang = 10;
+let lebar = 8;
+
+let keliling = 2 * (panjang + lebar);
+console.log("Keliling persegi panjang adalah : " +keliling);
