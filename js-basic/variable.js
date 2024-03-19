80c39ff909831931ecf8997fa5a85f162f5be019
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-19 21:17:17 +0700

    Membuat operator aritmatika dalam javascript

--- js-basic/variable.js
+++ js-basic/variable.js
@@ -6,3 +6,17 @@ console.log(str );
 
 var isCorrect = true;
 console.log(isCorrect);
+
+
+// operator dalam javascript
+
+var a = 15;
+var b = "abc";
+var total = a % b;
+console.log(total);
+
+/**
+ * Dalam Operator matematika ketika sebuah tipe data number menggunakan operasi (- , / , *, %) dengan tipe data string
+ * hasilnya akan Nan.
+ * Namun jika menggunakan operasi ( + ) akan menjadi concat
+ */
