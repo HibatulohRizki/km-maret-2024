b9b3245fc28f08bbd993d877d7c61e274c6773c9
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-19 21:24:26 +0700

    Membuat operator logic/perbandingan

--- js-basic/variable.js
+++ js-basic/variable.js
@@ -16,7 +16,21 @@ var total = a % b;
 console.log(total);
 
 /**
- * Dalam Operator matematika ketika sebuah tipe data number menggunakan operasi (- , / , *, %) dengan tipe data string
+ * Dalam Operator matematika ketika sebuah tipe data number menggunakan operasi (- , / , *, %) 
+ * dengan tipe data string
  * hasilnya akan Nan.
  * Namun jika menggunakan operasi ( + ) akan menjadi concat
  */
+
+// OPERATOR LOGIC
+
+var x = 15;
+var y = "15";
+var z = x == y;
+console.log(z);
+var v = x === y;
+console.log(v);
+
+/* Dalam operasi logic jika == hanya akan membandingkan valuenya saja sehingga hasilnya true
+Sedangkan === akan membanding value dan tipe datanya sehingga hasilnya false
+*/
