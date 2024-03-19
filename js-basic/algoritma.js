63cd683cd509ecb0b485faf57994d0a99eeb05e8
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-19 20:22:49 +0700

    merevisi rumus luas Permukaan tabung dan membuat rumus seperEmpat Volume Bola

--- js-basic/algoritma.js
+++ js-basic/algoritma.js
@@ -25,7 +25,17 @@ console.log("Keliling persegi panjang adalah : " +keliling);
 //  STUDY CASE 2
 
 let jariJari = 7;
+let tinggi = 5;
 let phi = 3.14;
 
-let luasPermukaanTabung = 2 * (phi * jariJari *jariJari);
+let luasPermukaanTabung =  2 * phi * jariJari * jariJari + 2 * phi * jariJari * tinggi;
 console.log("Luas permukaan tabung adalah : " +luasPermukaanTabung);
+
+//  STUDY CASE 3
+
+let r = 10;
+let pi = 3.14;
+
+let volumeBola = (4/3) * pi * r * r * r;
+let volumeSeperempatBola = (1/4) *  volumeBola;
+console.log("Volume bola adalah : "+volumeSeperempatBola) ;
