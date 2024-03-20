f54892cba7ee6464776eff3dc790510c42bf0f21
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-20 21:34:55 +0700

    menyelesaikan study case 2 yaitu jumlah faktor bilangan

--- js-basic/looping.js
+++ js-basic/looping.js
@@ -21,7 +21,17 @@
      * Tentukan jumlah faktor dari sebuah bilangan
      */
 
-    
+    let bilangan= 24;
+    let faktor=0;
+
+    for(let i = 1; i <= bilangan ; i++){
+        if(bilangan % i == 0){
+            faktor ++;
+        }
+    }
+    console.log("Faktor dari bilangan "+ bilangan + " adalah "+faktor);
+
+
     /**
      * Study case 3
      * 
