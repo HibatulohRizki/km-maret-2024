213f24e1a2ef1432e453d333aaf1ddcb0dbf420f
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-20 21:27:47 +0700

    Membuat file looping dan mengerjakan study case 1

---
+++ js-basic/looping.js
@@ -0,0 +1,30 @@
+    // Looping
+
+    /**
+     * Study case 1
+     * 
+     * Tampilkan 10 bilangan ganjil dan genap pertama
+     * 
+     */
+    let num = 10
+    for(let i = 1 ; i <= num ; i++){
+        if(i % 2 === 0){
+            console.log("Genap : " + i);
+        }else{
+            console.log("Ganjil : " + i);
+        }
+    }
+
+    /**
+     * Study case 2
+     * 
+     * Tentukan jumlah faktor dari sebuah bilangan
+     */
+
+    
+    /**
+     * Study case 3
+     * 
+     * Cek apakah bilangan prima
+     */
+
