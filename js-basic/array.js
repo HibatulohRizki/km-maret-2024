0f896180d98cd195570bc94d1bc59cbb9800ab46
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-21 20:25:35 +0700

    mengganti variabel pada study case 1

--- js-basic/array.js
+++ js-basic/array.js
@@ -1,16 +1,16 @@
 //study case 1
 
-let murid = [
+let students = [
     [1, "Vincent" , 3.5],
     [2, "Udin" , 3.0],
     [3,"Mamang", 2.1]
 ]
 
-for(let i = 0; i < murid.length ;i++){
-    if(murid[i][2] >= 3.0){
-        console.log(murid[i][0] + "."+murid[i][1] + ", IPK = "+murid[i][2]+ "Lulus");
+for(let i = 0; i < students.length ;i++){
+    if(students[i][2] >= 3.0){
+        console.log(students[i][0] + "."+students[i][1] + ", IPK = "+students[i][2]+ "Lulus");
     }else{
-        console.log(murid[i][0] + "."+murid[i][1] + ", IPK = "+murid[i][2]+ "Gagal");
+        console.log(students[i][0] + "."+students[i][1] + ", IPK = "+students[i][2]+ "Gagal");
     }
 }
 
