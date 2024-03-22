7adf20c102404ceb39a704836cf75ff923eb5328
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-22 19:53:56 +0700

    Membuat function untuk menghitung sebuah kalimat

--- js-basic/function.js
+++ js-basic/function.js
@@ -9,3 +9,29 @@ function checkNumber(num) {
 }
 
 console.log(checkNumber(10));
+
+/**
+ * Study case 2
+ * 
+ * buatlah 2 fungsi :
+ * megubah kalimat menjadi larik,
+ * menghitung jumlah elemen dalam larik
+ * 
+ * contoh masukan : "Hallo Selamat Pagi" akan memberikan hasil 3
+ * 
+ */
+
+
+function splitArray(str) {
+    return str.split(" ");
+    
+}
+function countElementInArray(kalimat){
+    let result = splitArray(kalimat);
+    let arrayLength = result.length;
+    return arrayLength;
+}
+let exampleSentence = "Halo Selamat Pagi";
+console.log(countElementInArray(exampleSentence));
+
+
