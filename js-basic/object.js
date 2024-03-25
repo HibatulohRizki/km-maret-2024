02f2d02664b0ad5f9b21e2ad4616c440bcf030ed
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-25 21:21:54 +0700

    Method in object

--- js-basic/object.js
+++ js-basic/object.js
@@ -11,3 +11,27 @@ function buah (arr){
 }
 
 console.log(buah(["Alpukat",30000,true]));
+
+
+/**
+ * method in object
+ */
+
+let  person={
+    nama : "Jihyo",
+    weight : 55,
+    height : 160,
+    hobbies : ["Membuat kaum adam klepek-klepek", "Beautiful"],
+
+    jihyoHobb: function(){
+        console.log("Jihyo Hobbies : ");
+        for(let i = 0; i < this.hobbies.length ;i++){
+            console.log(i + 1 + ". "+ this.hobbies[i]);
+        }
+    },
+    tambahHobby: function (hobbyName) {
+        this.hobbies.push(hobbyName);
+    },
+};
+person.jihyoHobb();
+    
