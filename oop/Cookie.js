aa96dd883a3f363dd68777b092e2b1778d62ad0b
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-04-01 22:09:25 +0700

    membuat file class cookie di file cookie

---
+++ oop/Cookie.js
@@ -0,0 +1,32 @@
+class Cookies {
+    constructor(id, name, price, ingredients, type){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+        this.type= type;
+        
+    }
+}
+class Chocolate extends Cookies{
+    constructor(id, name, price, ingredients,type,isSweet){
+        super(id, name, price,ingredients, type);
+        this.isSweet = isSweet ;
+    }
+}
+
+class Sweet extends Cookies{
+    constructor(id, name, price,ingredients,type,isSweet){
+        super(id, name, price, ingredients,type)
+        this.isSweet = isSweet;   
+    }
+}
+
+class Strawberry extends Cookies{
+    constructor(id,name,price,ingredients,type,isSweet){
+        super(id,name,price,ingredients,type);
+        this.isSweet= isSweet;
+    }
+}
+
+module.exports={ Chocolate, Sweet, Strawberry};
